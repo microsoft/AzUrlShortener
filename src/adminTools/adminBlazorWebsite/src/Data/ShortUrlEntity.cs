92f2a6b5cd70790009ab36cab44e0e9adbeb1784
fs366e2spm <52791126+fs366e2spm@users.noreply.github.com>
2020-06-13 16:44:01 +0200

    Update ShortUrlEntity.cs

--- src/adminTools/adminBlazorWebsite/src/Data/ShortUrlEntity.cs
+++ src/adminTools/adminBlazorWebsite/src/Data/ShortUrlEntity.cs
@@ -32,7 +32,7 @@ namespace  adminBlazorWebsite.Data
 
             var lenght = Url.ToString().Length;
             if (lenght >= 50){
-                return string.Concat(Url.Substring(1,50), "...");
+                return string.Concat(Url.Substring(0,49), "...");
             }
             return Url;
         }
