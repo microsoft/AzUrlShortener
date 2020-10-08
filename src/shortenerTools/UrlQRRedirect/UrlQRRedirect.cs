4bf9c76f20f14777f85733fc0ce6351cffa83c51
Michael Humpl <m.humpl.enbw@outlook.de>
2020-10-08 18:21:15 +0200

    changed method from async to sync

--- src/shortenerTools/UrlQRRedirect/UrlQRRedirect.cs
+++ src/shortenerTools/UrlQRRedirect/UrlQRRedirect.cs
@@ -7,26 +7,26 @@ using System.Net;
 using System.Net.Http;
 using Cloud5mins.domain;
 using Microsoft.Extensions.Configuration;
-using System.Net.Http.Headers;
 
 namespace Cloud5mins.Function
 {
     public static class UrlQRRedirect
     {
         [FunctionName("UrlQRRedirect")]
-        public static async Task<HttpResponseMessage> Run(
+        public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "UrlQRRedirect/{shortUrl}")] HttpRequestMessage req,
             string shortUrl, 
             ExecutionContext context,
             ILogger log)
         {
-            log.LogInformation($"C# HTTP trigger function processed for Url: {shortUrl}");
 
-            var redirectUrl = "http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data="+WebUtility.UrlEncode(req.RequestUri.AbsoluteUri)+"&amp;qzone=0&amp;margin=0&amp;size=250x250&amp;ecc=L";
+           log.LogInformation($"C# HTTP trigger function processed for Url: {shortUrl}");
 
-            var res = req.CreateResponse(HttpStatusCode.Redirect);
-            res.Headers.Add("Location", redirectUrl);
-            return res;
-        }
-  }
+           var redirectUrl = "http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data="+WebUtility.UrlEncode(req.RequestUri.AbsoluteUri)+"&amp;qzone=0&amp;margin=0&amp;size=250x250&amp;ecc=L";
+
+           var res = req.CreateResponse(HttpStatusCode.Redirect);
+           res.Headers.Add("Location", redirectUrl);
+           return res;
+           }
+    }      
 }
