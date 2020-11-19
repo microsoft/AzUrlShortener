df9a950618b0fd59043112a7b4ff53e16c6fe1ae
Chris Renton <iammrrenton@gmail.com>
2020-11-19 10:19:41 +0000

    Update Utility.cs

--- src/shortenerTools/Domain/Utility.cs
+++ src/shortenerTools/Domain/Utility.cs
@@ -1,21 +1,23 @@
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Cloud5mins.domain
 {
     public static class Utility
     {
-        //reshuffled for randomisation, same unique characters just jumbled up, use your own for security
-        private const string ConversionCode = "aoq6lewdfit0nbvp3ukz8mc941gsj57r2hyx";
+        //reshuffled for randomisation, same unique characters just jumbled up, you can replace with your own version
+        private const string ConversionCode = "FjTG0s5dgWkbLf_8etOZqMzNhmp7u6lUJoXIDiQB9-wRxCKyrPcv4En3Y21aASHV";
         private static readonly int Base = ConversionCode.Length;
-        private const int MinVanityLength = 4;
+        //sets the length of the unique code to add to vanity
+        private const int MinVanityCodeLength = 5;
 
         public static async Task<string> GetValidEndUrl(string vanity, StorageTableHelper stgHelper)
         {
             if (string.IsNullOrEmpty(vanity))
             {
                 var newKey = await stgHelper.GetNextTableId();
-                string getCode() => Encode(newKey, MinVanityLength);
+                string getCode() => Encode(newKey);
                 return string.Join(string.Empty, getCode());
             }
             else
@@ -24,31 +26,34 @@ namespace Cloud5mins.domain
             }
         }
 
-        public static string Encode(int i, int minVanityLength)
+        public static string Encode(int i)
         {
             if (i == 0)
                 return ConversionCode[0].ToString();
-            var s = string.Empty;
-            while (i > 0)
-            {
-                s += ConversionCode[i % Base];
-                i = i / Base;
-                //if we setting a minimum length just extend the code accordingly
-                if (minVanityLength > 0)
-                {
-                    while (s.Length < minVanityLength)
-                    {
-                        s += ConversionCode[s.Length % Base];
-                    }
-                }
-            }
 
-            return string.Join(string.Empty, s.Reverse());
+            return GenerateUniqueRandomToken(i);
         }
 
         public static string GetShortUrl(string host, string vanity)
         {
             return host + "/" + vanity;
+        }
+
+        // generates a unique, random, and alphanumeric token for the use as a url 
+        //(not entirely secure but not sequential so generally not guessable)
+        public static string GenerateUniqueRandomToken(int uniqueId)
+        {
+            using (var generator = new RNGCryptoServiceProvider())
+            {
+                //minimum size I would suggest is 5, longer the better but we want short URLs!
+                var bytes = new byte[MinVanityCodeLength];
+                generator.GetBytes(bytes);
+                var chars = bytes
+                    .Select(b => ConversionCode[b % ConversionCode.Length]);
+                var token = new string(chars.ToArray());
+                var reversedToken = string.Join(string.Empty, token.Reverse());
+                return uniqueId + reversedToken;
+            }
         }
     }
 }
