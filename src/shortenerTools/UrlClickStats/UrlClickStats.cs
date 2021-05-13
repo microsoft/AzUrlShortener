ed30cb22628049d41320ffe47dbbbf6b99a020ae
apetkovic <aleks@beaconsoftco.com>
2021-05-13 18:15:59 -0400

    Switch HttpResponseMessage to IActionResult
    Done for .Net Core v3 support

--- src/shortenerTools/UrlClickStats/UrlClickStats.cs
+++ src/shortenerTools/UrlClickStats/UrlClickStats.cs
@@ -3,17 +3,17 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
 using System.Net.Http;
 using Cloud5mins.domain;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cloud5mins.Function
 {
     public static class UrlClickStats
     {
         [FunctionName("UrlClickStats")]
-        public static async Task<HttpResponseMessage> Run(
+        public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, 
         ILogger log, 
         ExecutionContext context)
@@ -23,13 +23,13 @@ namespace Cloud5mins.Function
             // Validation of the inputs
             if (req == null)
             {
-                return req.CreateResponse(HttpStatusCode.NotFound);
+                return new NotFoundResult();
             }
 
             UrlClickStatsRequest input = await req.Content.ReadAsAsync<UrlClickStatsRequest>();
             if (input == null)
             {
-                return req.CreateResponse(HttpStatusCode.NotFound);
+                return new NotFoundResult();
             }
 
             var result = new ClickStatsEntityList();
@@ -48,10 +48,10 @@ namespace Cloud5mins.Function
             catch (Exception ex)
             {
                 log.LogError(ex, "An unexpected error was encountered.");
-                return req.CreateResponse(HttpStatusCode.BadRequest, ex);
+                return new BadRequestObjectResult(ex);
             }
 
-            return req.CreateResponse(HttpStatusCode.OK, result);
+            return new OkObjectResult(result);
         }
     }
 }
