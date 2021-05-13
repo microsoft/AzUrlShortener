75da309215d907b107d1a774564a3705f7104c0c
apetkovic <aleks@beaconsoftco.com>
2021-05-13 18:17:16 -0400

    Switch HttpResponseMessage to IActionResult
    Done for .Net Core v3 support

--- src/shortenerTools/UrlList/UrlList.cs
+++ src/shortenerTools/UrlList/UrlList.cs
@@ -20,20 +20,20 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
 using System.Net.Http;
 using Cloud5mins.domain;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cloud5mins.Function
 {
     public static class UrlList
     {
         [FunctionName("UrlList")]
-        public static async Task<HttpResponseMessage> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequestMessage req, 
-        ILogger log, 
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequestMessage req,
+        ILogger log,
         ExecutionContext context)
         {
             log.LogInformation($"C# HTTP trigger function processed this request: {req}");
@@ -45,24 +45,25 @@ namespace Cloud5mins.Function
                 .AddEnvironmentVariables()
                 .Build();
 
-            StorageTableHelper stgHelper = new StorageTableHelper(config["UlsDataStorage"]); 
+            StorageTableHelper stgHelper = new StorageTableHelper(config["UlsDataStorage"]);
 
             try
             {
-               result.UrlList = await stgHelper.GetAllShortUrlEntities();
+                result.UrlList = await stgHelper.GetAllShortUrlEntities();
                 result.UrlList = result.UrlList.Where(p => !(p.IsArchived ?? false)).ToList();
-               var host = req.RequestUri.GetLeftPart(UriPartial.Authority); 
-               foreach(ShortUrlEntity url in result.UrlList){
-                   url.ShortUrl = Utility.GetShortUrl(host, url.RowKey);
-               }
+                var host = req.RequestUri.GetLeftPart(UriPartial.Authority);
+                foreach (ShortUrlEntity url in result.UrlList)
+                {
+                    url.ShortUrl = Utility.GetShortUrl(host, url.RowKey);
+                }
             }
             catch (Exception ex)
             {
                 log.LogError(ex, "An unexpected error was encountered.");
-                return req.CreateResponse(HttpStatusCode.BadRequest, ex);
+                return new BadRequestObjectResult(ex);
             }
 
-            return req.CreateResponse(HttpStatusCode.OK, result);
+            return new OkObjectResult(result);
         }
     }
 }
