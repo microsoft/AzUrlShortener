350645f788d2575974ba1f90f724fccabbd30042
Frank <fboucher@outlook.com>
2021-07-15 18:43:24 -0400

    Updating code to use new security
    Ref to Issue #320

--- src/app/TinyBlazorAdmin/Program.cs
+++ src/app/TinyBlazorAdmin/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TinyBlazorAdmin.Data;
 using Syncfusion.Blazor;
+using Microsoft.Azure.Functions.Authentication.WebAssembly;
 
 namespace TinyBlazorAdmin
 {
@@ -15,37 +16,8 @@ namespace TinyBlazorAdmin
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            // set up a delegate to get function token
-            static string functionEndpoint(WebAssemblyHostBuilder builder) =>
-                builder.Configuration
-                    .GetSection(nameof(UrlShortenerSecuredService))
-                    .GetValue<string>(nameof(AzFuncAuthorizationMessageHandler.Endpoint));
-
-            // sets up AAD + user_impersonation to access functions.
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                options.ProviderOptions
-                .DefaultAccessTokenScopes.Add("user.read");
-                options.ProviderOptions
-                .AdditionalScopesToConsent.Add($"{functionEndpoint(builder)}user_impersonation");
-                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-            });
-
-
-            // set up DI
-            builder.Services.AddTransient<AzFuncAuthorizationMessageHandler>();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            // configure the client to talk to the Azure Functions endpoint.
-            builder.Services.AddHttpClient(nameof(UrlShortenerSecuredService),
-                client =>
-                {
-                    client.BaseAddress = new Uri(functionEndpoint(builder));
-                }).AddHttpMessageHandler<AzFuncAuthorizationMessageHandler>();
-
-            builder.Services.AddTransient<UrlShortenerSecuredService>();
-            builder.Services.AddTransient<AzFuncClient>();
+            builder.Services.AddStaticWebAppsAuthentication();
 
             //Add SycnFusion Controls
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzgwNjM5QDMxMzgyZTMzMmUzMGhPbXloTFpmTVFQTEgrMUZ2NjZONkFEZmhLOG16RkhYSkYyZmZpOHRVUkU9"); 
