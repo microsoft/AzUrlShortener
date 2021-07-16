28177c0a9e969849626ffc4a43bc58d90762cde0
Frank <fboucher@outlook.com>
2021-07-16 11:25:57 -0400

    adding service

--- src/app/TinyBlazorAdmin/Program.cs
+++ src/app/TinyBlazorAdmin/Program.cs
@@ -16,6 +16,21 @@ namespace TinyBlazorAdmin
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            //  // set up a delegate to get function token
+            // static string functionEndpoint(WebAssemblyHostBuilder builder) =>
+            //     builder.Configuration
+            //         .GetSection(nameof(UrlShortenerSecuredService));
+
+            // configure the client to talk to the Azure Functions endpoint.
+            // builder.Services.AddHttpClient(nameof(UrlShortenerSecuredService),
+            //     client =>
+            //     {
+            //         client.BaseAddress = new Uri(functionEndpoint(builder));
+            //     })
+
+
+            builder.Services.AddTransient<UrlShortenerSecuredService>();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddStaticWebAppsAuthentication();
 
