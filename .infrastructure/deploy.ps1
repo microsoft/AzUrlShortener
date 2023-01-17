d95695163a21f48087661c0d7892c700b1581826
Chris House <chhouse@microsoft.com>
2023-01-17 13:32:00 -0600

    yeeeeaaahhh

--- .infrastructure/deploy.ps1
+++ .infrastructure/deploy.ps1
@@ -1,3 +1,4 @@
 $deployName = Get-Date -Format "yyyyMMddHHmmss"
+$resourceGroup = "URL-SHORTENER"
 
 az deployment group create -n $deployName -g $resourceGroup --template-file .\main.bicep --parameters '@.\parameters\development\parameters.json'
