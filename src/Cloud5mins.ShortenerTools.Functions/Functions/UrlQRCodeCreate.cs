f9927512391d6b8fd6630fc5a699d5b4368cdb51
Harry Kimpel <harrykimpel@hotmail.com>
2024-12-05 14:42:38 +0100

    Adding QR Code create function
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- src/Cloud5mins.ShortenerTools.Functions/Functions/UrlQRCodeCreate.cs
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/UrlQRCodeCreate.cs
@@ -30,10 +30,27 @@ namespace Cloud5mins.Function
             ExecutionContext context) //,
             //ILogger log)
         {
-
            //log.LogInformation($"C# HTTP trigger function processed for Url: {shortUrl}");
 
-           var redirectUrl = "http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data="+WebUtility.UrlEncode(req.RequestUri.AbsoluteUri)+"&qzone=0&margin=0&size=250x250&ecc=L";
+           ShortRequest input;
+
+           // Validation of the inputs
+            if (req == null)
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            using (var reader = new StreamReader(req.Body))
+            {
+                var strBody = await reader.ReadToEndAsync();
+                input = JsonSerializer.Deserialize<ShortRequest>(strBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                if (input == null)
+                {
+                    return req.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+
+           var redirectUrl = "http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data="+WebUtility.UrlEncode(input.Url)+"&qzone=0&margin=0&size=250x250&ecc=L";
 
            var res = req.CreateResponse(HttpStatusCode.Redirect);
            res.Headers.Add("Location", redirectUrl);
